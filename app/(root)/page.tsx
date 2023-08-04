b4185a7ced3d74e7ed3419f449b5d8b05d3ea7c4
zhang yuyang <zhangyuyang1211@gmail.com>
2023-08-04 23:48:52 +0800

    Modal Components Finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,19 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/useStoreModal";
 import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { useEffect } from "react";
 
 export default function SetupPage() {
-  return (
-    <div className="underline">
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page s</div>;
 }
